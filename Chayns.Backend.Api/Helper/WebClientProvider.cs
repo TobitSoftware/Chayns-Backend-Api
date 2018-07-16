7f4579d8087700b9e14f7907ccd08620e0ef970f
Sebastian Barton <sebastian.barton89@gmail.com>
2018-07-16 15:56:20 +0200

    #6: Add WebClientProvider to reuse HttpClient

---
+++ Chayns.Backend.Api/Helper/WebClientProvider.cs
@@ -0,0 +1,15 @@
+﻿using System.Net.Http;
+using System.Net.Http.Headers;
+
+namespace Chayns.Backend.Api.Helper
+{
+    internal static class WebClientProvider
+    {
+        private static readonly HttpClient Client = new HttpClient
+        {
+            DefaultRequestHeaders = { UserAgent = { new ProductInfoHeaderValue("ChaynsBackendApi", "2")}}
+        };
+
+        internal static HttpClient GetClient() => Client;
+    }
+}
